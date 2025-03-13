c5c9c86f84784b3999537125e07bb4e2b8caeb1d
massonskyi <gavrin.01@bk.ru>
2025-03-13 17:33:32 +0300

    init 1.0.2

--- build/git_version.h
+++ build/git_version.h
@@ -1,8 +1,8 @@
 #ifndef GIT_VERSION_H
 #define GIT_VERSION_H
 
-#define GIT_BRANCH "unknown"
-#define GIT_COMMIT_HASH "unknown"
-#define GIT_COMMIT_DATE "unknown"
+#define GIT_BRANCH "master"
+#define GIT_COMMIT_HASH "5f385f9"
+#define GIT_COMMIT_DATE "2025-03-13"
 
 #endif // GIT_VERSION_H
